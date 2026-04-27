cd558fc3ed91179f7571931c807624e70b0833a0
Rishabh Singh <singhrishabhj19@gmail.com>
2026-04-27 05:58:28 +0530

    Add Java program to check if number is greater than 100

---
+++ lect1/Assignments/A5_GreaterThan100.java
@@ -0,0 +1,12 @@
+public class A5_GreaterThan100 {
+    public static void main(String[] args) {
+        
+        int num = 900;
+        
+        if (num > 100){
+            System.out.println("The number is greater than 100.");
+        }else{
+            System.out.println("The number is not greater than 100.");
+        }
+    }
+}
