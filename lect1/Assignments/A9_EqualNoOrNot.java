43aabe815acbca476962200eb4a22b93191d5c35
Rishabh Singh <singhrishabhj19@gmail.com>
2026-04-27 06:12:59 +0530

    Add Java program to check if two numbers are equal

---
+++ lect1/Assignments/A9_EqualNoOrNot.java
@@ -0,0 +1,13 @@
+public class A9_EqualNoOrNot {
+
+    public static void main(String[] args) {
+        int num1 = 90 ;
+        int num2 = 90 ;
+
+        if (num1 == num2){
+            System.out.println("The number one : " + num1 + " and number two " + num2 +" are equal. ");
+        } else {
+            System.out.println("The number one : " + num1 + " and number two " + num2 +" are not equal. ");
+        }
+    }
+}
