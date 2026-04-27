91c7f7c697d48c8d91414c7d679e1037bdb449d0
Rishabh Singh <singhrishabhj19@gmail.com>
2026-04-27 05:44:20 +0530

    Add PositiveOrNegative and OddEven Java programs

---
+++ lect1/Assignments/A1_PositiveOrnegative.java
@@ -0,0 +1,13 @@
+public class A1_PositiveOrnegative {
+    public static void main(String[] args) {
+    int n = 1;
+
+    if(n > 0){
+        System.out.println("The number is positive.");
+    }else if (n == 0){
+         System.out.println("The number is zero.");
+    }else{
+         System.out.println("The number is negative.");
+    }
+    }  
+}
---
+++ lect1/Assignments/A2_OddEven.java
@@ -0,0 +1,12 @@
+public class A2_OddEven{
+    public static void main(String[] args) {
+        
+        int n = 77;
+
+        if( n % 2 == 0){
+            System.out.println("The number is Even");
+        }else {
+            System.out.println("The number is Odd");
+        }
+    }
+}
