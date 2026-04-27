66eeaac0ac1b76bfbbfca9ddcbd9c1d5ce6e660e
Rishabh Singh <singhrishabhj19@gmail.com>
2026-04-27 05:48:19 +0530

    Add EligibleToVote Java program

---
+++ lect1/Assignments/A3_EligibleToVote.java
@@ -0,0 +1,13 @@
+public class A3_EligibleToVote {
+    public static void main(String[] args) {
+    
+    int age = 59;
+
+    if (age >= 18){
+        System.out.println("The person is eligible to vote");
+    }else{
+        System.out.print("The person is not eleibile to vote");
+    }
+
+    }
+}
