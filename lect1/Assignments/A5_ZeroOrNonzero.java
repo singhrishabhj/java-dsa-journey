94cb7fe04069f8ca9747292f4ecb138e24d6218e
Rishabh Singh <singhrishabhj19@gmail.com>
2026-04-27 05:55:29 +0530

    Add Java program to check zero or non-zero number

---
+++ lect1/Assignments/A5_ZeroOrNonzero.java
@@ -0,0 +1,12 @@
+public class A5_ZeroOrNonzero {
+
+     public static void main(String[] args) {
+        int num = 99;
+
+        if (num == 0){
+            System.out.println("The number is zero");
+        }else{
+            System.out.println("The number is non zero");
+        }
+     }
+}
