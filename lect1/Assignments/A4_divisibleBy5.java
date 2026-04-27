a751ea13ff09f26e73c5d08ac32f506174916485
Rishabh Singh <singhrishabhj19@gmail.com>
2026-04-27 05:51:37 +0530

    Add Java program to check if number is divisible by 5

---
+++ lect1/Assignments/A4_divisibleBy5.java
@@ -0,0 +1,11 @@
+public class A4_divisibleBy5 {
+    public static void main(String[] args) {
+        int num = 90;
+
+        if (num % 5 == 0){
+            System.out.println("The number " + num +" is divisble by 5");
+        }else {
+            System.out.println("The number " + num +" is not divisble by 5");
+        }
+    }
+}
